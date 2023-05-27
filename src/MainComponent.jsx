a53eda1cb8afb254a25ba9efbf6d1a806aeb18d5
williammilkyan <williammilkyan@hotmail.com>
2023-05-26 17:38:03 -0700

    try again

--- src/MainComponent.jsx
+++ src/MainComponent.jsx
@@ -6,14 +6,14 @@ export function MainComponent () {
           <p className="description">A Frontend Web Developer </p>
           <p className="description">base in Vancouver, BC, Canada.</p>
      </div>
-          <img className="myImage" src="./pic/william.jpg" />
+          <img className="myImage" src="/pic/william.jpg" />
      <div>
           
           <div className="skillContainer" id="skill">
-               <img className="skillBox" src="./pic/htmlicon.png"/>
-               <img className="skillBox" src="./pic/cssicon.png"/>
-               <img className="skillBox" src="./pic/javascripticon.png"/>
-               <img className="skillBox" src="./pic/reacticon.png"/>
+               <img className="skillBox" src="/pic/htmlicon.png"/>
+               <img className="skillBox" src="/pic/cssicon.png"/>
+               <img className="skillBox" src="/pic/javascripticon.png"/>
+               <img className="skillBox" src="/pic/reacticon.png"/>
           </div>
         </div>
     </div>
